aaa3c0a26b391807cda277c91f3130f2d62696db
fedpre <fed.pregnolato@gmail.com>
2023-12-24 04:46:40 -0700

    Initial Commit

---
+++ AnimationPortfolioiOSApp/AnimationPortfolioiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationPortfolioiOSAppApp.swift
+//  AnimationPortfolioiOSApp
+//
+//  Created by Federico Pregnolato on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationPortfolioiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
